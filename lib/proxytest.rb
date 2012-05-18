5a298ebfb1375b2ab6c613f3dee772e67ca6479b
Rich Healey <richard.healey@99designs.com>
2012-05-18 12:59:33 +1000

    Don't create an instance till we're ready, adjust paths

--- lib/proxytest.rb
+++ lib/proxytest.rb
@@ -1,10 +1,6 @@
 require 'test/unit'
 require 'net/http'
 
-require File.expand_path("../helpers/backend", __FILE__)
-require File.expand_path("../helpers/varnish", __FILE__)
-require File.expand_path("../helpers/response", __FILE__)
-
-# Seed the queue by giving it a port
-
-SingletonQueue.new
+require File.expand_path("../proxytest/helpers/backend", __FILE__)
+require File.expand_path("../proxytest/helpers/varnish", __FILE__)
+require File.expand_path("../proxytest/helpers/response", __FILE__)
